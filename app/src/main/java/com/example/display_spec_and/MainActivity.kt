5b5ba990b137cf49d444163c55b4acbc48d5b228
hansol-yang <hansolyan9@sonsedu.net>
2019-06-26 18:05:23 +0900

    Add feature 0) display Screen Size(small, normal, large, xlarge)

--- app/src/main/java/com/example/display_spec_and/MainActivity.kt
+++ app/src/main/java/com/example/display_spec_and/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.example.display_spec_and
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity :
+    AppCompatActivity(),
+    MainContract.View {
+
+    private val presenter: MainContract.Presenter by lazy {
+        MainPresenter(this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        presenter.onViewCreate()
     }
+
+    override fun showScreenSize() {
+        tv_screen_size.text = getScreenSize()
+    }
+
+    override fun onDestroy() {
+        presenter.onViewDestroy()
+        super.onDestroy()
+    }
+
+    private fun getScreenSize(): String =
+        when (resources.configuration.screenLayout and Configuration.SCREENLAYOUT_SIZE_MASK) {
+            Configuration.SCREENLAYOUT_SIZE_SMALL -> "Small"
+            Configuration.SCREENLAYOUT_SIZE_NORMAL -> "Normal"
+            Configuration.SCREENLAYOUT_SIZE_LARGE -> "Large"
+            Configuration.SCREENLAYOUT_SIZE_XLARGE -> "X-Large"
+            else -> "ERROR"
+        }
 }
