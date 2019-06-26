4161dc2fd17f3bc5b17f973a101f8bae1a757fa3
HansolYang <hansolyan9@sonsedu.net>
2019-06-26 17:43:43 +0900

    First commit

---
+++ app/src/main/java/com/example/display_spec_and/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.display_spec_and
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
